f7aa20f8349865a140d0002adf48cb3ccba99661
Poutchy <enzo.mauti@laposte.net>
2024-04-17 14:19:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# import polyscope as ps
+
+# from recherche.wavefront import load_obj
+
+# obj = load_obj( 'Mesh/spot.obj')
+# ps.init()
+# ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces() )
+# ps.show()
+
+
+import polyscope as ps
+import numpy as np
+
+from recherche.wavefront import load_obj
+
+name='Mesh/bunnyhead'
+obj_name=name+'.obj'
+obj = load_obj( obj_name )
+ps.init()
+ps_mesh = ps.register_surface_mesh(name, obj.only_coordinates(), obj.only_faces() )
+bdry  = obj.numpy_boundary_edges()
+ps_net= ps.register_curve_network("boundary", obj.only_coordinates(), bdry )
+ps.show()
